9db1654cd4c8d9e0c93af559b478f124ab8a8d52
musiur <musiur.opu@gmail.com>
2023-02-27 19:57:59 +0600

    first commit

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,30 @@
+
+const db = require("../models");
+const User = db.user;
+
+exports.allAccess = (req, res) => {
+  User.find({},(err, users) => {
+    var userMap = {};
+
+    users.forEach(function(user) {
+      userMap[user._id] = user;
+    });
+
+    res.status(200).send(userMap);  
+  });
+};
+
+
+exports.userBoard = (req, res) => {
+  User.findOne({_id: "63f52aa29f997a9ccc44a1e0"}, (err, users) => {
+    res.status(200).send(users)
+  })
+};
+
+exports.adminBoard = (req, res) => {
+  res.status(200).send("Admin Content.");
+};
+
+exports.moderatorBoard = (req, res) => {
+  res.status(200).send("Moderator Content.");
+};
